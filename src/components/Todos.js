85694c47829a5837656664b20dded61975b6e444
Shubham <shubh.chavhan00@gmail.com>
2022-05-06 11:27:38 +0530

    added form for the input

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 const Todos = () => {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div>
       <h1>Shubham's Todo List</h1>
+      <form>
+        <input type="text" />
+        <button type="Submit">Add Todo</button>
+      </form>
     </div>
   );
 };

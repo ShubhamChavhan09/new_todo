156fc7bad9d8ad3f38bd8b1af68a05e956667874
Shubham <shubh.chavhan00@gmail.com>
2022-05-06 11:24:08 +0530

    default

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
-      <h1>Shubham Chavhan</h1>
+      <Todos />
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Todos = () => {
+  const [todo, setTodo] = useState("");
+  return (
+    <div>
+      <h1>Shubham's Todo List</h1>
+    </div>
+  );
+};
+
+export default Todos;
